72d51808ff4f5fcd85ecb3f84b6fa60efc2d0549
Hugo Silva <jhssilva96@gmail.com>
2022-04-01 16:44:19 +0100

    created History section
    Created the history section.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Slide from "./components/Slide";
+import History from "./components/History";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Slide />
+      <History />
     </div>
   );
 }
---
+++ my-app/src/components/History.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import award1 from "../resources/img/awards/3.jpg";
+
+const History = () => {
+  return (
+    <section id="about" className="about anchor pt-4 pb-3">
+      <div
+        id="about-global"
+        data-aos="fade-up"
+        className="container container-fluid"
+      >
+        <div className="section-title pb-5">
+          <h2 className="text-center">
+            <span id="one">A nossa História</span>
+          </h2>
+        </div>
+        <div className="row">
+          <div className="col-11 mx-auto col-md-11 col-lg-10 justify-content-center">
+            <div className="row">
+              <div className="col-12 col-sm-12 col-md-8 col-lg-9">
+                <p className="about-text text-justify">
+                  <b className="about-text-bold">Restaurante Chuva</b> com mais
+                  de 50 anos de serviço (1958) é uma referência incontornável da
+                  cozinha regional minhota. A qualidade dos pratos confecionados
+                  já foi por diversas vezes reconhecida com prémios e
+                  dinstinções. Acompanhado por uma adega com uma seleção de
+                  vinhos vindos das mais prestigiadas cooperativas do país.
+                  <br />
+                </p>
+                <p className="about-text text-justify">
+                  <b className="about-text-bold">
+                    As melhores especialidades da cozinha tradicional portuguesa
+                    e da cozinha regional minhota estão à sua espera no
+                    Restaurante Chuva!
+                  </b>
+                  <br />
+                </p>
+              </div>
+              <div className="col col-md-4 col-lg-3 d-none d-md-block">
+                <img
+                  alt="Maria Alice"
+                  src={award1}
+                  className="img-fluid about-pic"
+                />
+                <p className="about-pic-label d-flex justify-content-center">
+                  Maria Alice
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default History;
